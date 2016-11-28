949b27224727cc558570d259052d00b910e4686b
Alexander Koch <josef.koch@gmail.com>
2016-11-28 20:14:02 +0100

    Add test for KeyNotFoundException

---
+++ akkabase-core/src/test/java/org/ak80/akkabase/KeyNotFoundExceptionTest.java
@@ -0,0 +1,36 @@
+package org.ak80.akkabase;
+
+import org.ak80.akkabase.test.Builder;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SuppressWarnings("PMD.MethodNamingConventions")
+public class KeyNotFoundExceptionTest {
+
+  @Test
+  public void create_new_then_get_returns_key_value() {
+    // Given
+    String key = Builder.aKey();
+
+    // When
+    KeyNotFoundException keyNotFoundException = new KeyNotFoundException(key);
+
+    // Then
+    assertThat(keyNotFoundException.getKey(), is(key));
+  }
+
+  @Test
+  public void toString_returns_key_value_in_string() {
+    // Given
+    String key = Builder.aKey();
+
+    // When
+    KeyNotFoundException keyNotFoundException = new KeyNotFoundException(key);
+
+    // Then
+    assertThat(keyNotFoundException.toString(), is(KeyNotFoundException.class.getName()));
+  }
+
+}
